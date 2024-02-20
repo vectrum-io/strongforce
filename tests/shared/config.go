0c502832db09618d147ef9f2674381152a2e6197
Dennis Hoffmann <d.hoffmann@edudip.com>
2024-02-20 23:54:13 +0100

    feat: use some arbitrary port for postgres to void conflicts during tests
    Signed-off-by: Dennis Hoffmann <d.hoffmann@edudip.com>

--- tests/shared/config.go
+++ tests/shared/config.go
@@ -2,6 +2,6 @@ package sharedtest
 
 var (
 	MySQLDSN    = "strongforce:strongforce@tcp(127.0.0.1:65001)/strongforce?charset=utf8mb4&multiStatements=true&parseTime=true"
-	PostgresDSN = "postgresql://strongforce:strongforce@127.0.0.1:5432/strongforce?sslmode=disable"
+	PostgresDSN = "postgresql://strongforce:strongforce@127.0.0.1:65002/strongforce?sslmode=disable"
 	NATS        = "127.0.0.1:23945"
 )
